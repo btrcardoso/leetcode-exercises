01848ea3e3b793c214d4e7f5a70fca3c576db77f
Beatriz Cardoso <beatriz.cardoso@euentrego.com>
2026-01-16 20:14:46 -0300

    BinaryTreeLevelOrderTraversal better

--- src/BinaryTreeLevelOrderTraversal.java
+++ src/BinaryTreeLevelOrderTraversal.java
@@ -19,61 +19,104 @@ public class BinaryTreeLevelOrderTraversal {
             this.right = right;
         }}
 
-    public <T> boolean isNotEmpty(List<T> list) {
-        return !list.isEmpty();
-    }
-
-    public boolean isValidNode(TreeNode treeNode) {
-        return treeNode != null;
-    }
-
     public List<List<Integer>> levelOrder(TreeNode root) {
 
         if (root == null) {
-            return  new ArrayList<List<Integer>>();
+            return new ArrayList<List<Integer>>();
         }
 
-        List<List<Integer>> listOfLevels = new ArrayList<List<Integer>>();
-        List<Integer> level = new ArrayList<>(List.of(root.val)); // a level is a list of values
-        listOfLevels.add(level);
+        List<TreeNode> queue = new ArrayList<TreeNode>();
+        queue.add(root);
 
-        List<List<TreeNode>> queueOfGraphs = new ArrayList<List<TreeNode>>();
-        List<TreeNode> graph = new ArrayList<>(List.of(root)); // a graph is an amount of nodes which is in the same level
-        queueOfGraphs.add(graph);
+        List<List<Integer>> levels = new ArrayList<List<Integer>>();
+        levels.add(List.of(root.val));
 
-        List<TreeNode> currentGraph = null;
-        TreeNode currentNode = null;
+        TreeNode currentNode;
+        while (!queue.isEmpty()) {
 
-        while (isNotEmpty(queueOfGraphs)) {
+            List<Integer> level = new ArrayList<Integer>();
+            int queueSize = queue.size();
 
-            currentGraph = queueOfGraphs.remove(0);
-            level = new ArrayList<Integer>();          // start to look at a new level
-            graph = new ArrayList<TreeNode>();         // start to build a new queue for the next level
+            for (int i = 0 ; i < queueSize; i ++){  // this is necessary to be sure we are only seeing the parents of the current level
+                currentNode = queue.remove(0);
 
-            while (isNotEmpty(currentGraph)) {
-
-                currentNode = currentGraph.remove(0);
-
-                if (isValidNode(currentNode.left)) {
+                if (currentNode.left != null) {
+                    queue.add(currentNode.left);
                     level.add(currentNode.left.val);
-                    graph.add(currentNode.left);
                 }
 
-                if (isValidNode(currentNode.right)) {
+                if (currentNode.right != null) {
+                    queue.add(currentNode.right);
                     level.add(currentNode.right.val);
-                    graph.add(currentNode.right);
                 }
 
             }
 
-            if (isNotEmpty(level)) {
-                listOfLevels.add(level);
-                queueOfGraphs.add(graph);
+            if (!level.isEmpty()){
+                levels.add(level);
             }
 
         }
 
+        return levels;
 
-        return listOfLevels;
     }
+
+//    public <T> boolean isNotEmpty(List<T> list) {
+//        return !list.isEmpty();
+//    }
+//
+//    public boolean isValidNode(TreeNode treeNode) {
+//        return treeNode != null;
+//    }
+//
+//    public List<List<Integer>> levelOrder(TreeNode root) {
+//
+//        if (root == null) {
+//            return  new ArrayList<List<Integer>>();
+//        }
+//
+//        List<List<Integer>> listOfLevels = new ArrayList<List<Integer>>();
+//        List<Integer> level = new ArrayList<>(List.of(root.val)); // a level is a list of values
+//        listOfLevels.add(level);
+//
+//        List<List<TreeNode>> queueOfGraphs = new ArrayList<List<TreeNode>>();
+//        List<TreeNode> graph = new ArrayList<>(List.of(root)); // a graph is an amount of nodes which is in the same level
+//        queueOfGraphs.add(graph);
+//
+//        List<TreeNode> currentGraph = null;
+//        TreeNode currentNode = null;
+//
+//        while (isNotEmpty(queueOfGraphs)) {
+//
+//            currentGraph = queueOfGraphs.remove(0);
+//            level = new ArrayList<Integer>();          // start to look at a new level
+//            graph = new ArrayList<TreeNode>();         // start to build a new queue for the next level
+//
+//            while (isNotEmpty(currentGraph)) {
+//
+//                currentNode = currentGraph.remove(0);
+//
+//                if (isValidNode(currentNode.left)) {
+//                    level.add(currentNode.left.val);
+//                    graph.add(currentNode.left);
+//                }
+//
+//                if (isValidNode(currentNode.right)) {
+//                    level.add(currentNode.right.val);
+//                    graph.add(currentNode.right);
+//                }
+//
+//            }
+//
+//            if (isNotEmpty(level)) {
+//                listOfLevels.add(level);
+//                queueOfGraphs.add(graph);
+//            }
+//
+//        }
+//
+//
+//        return listOfLevels;
+//    }
 }
